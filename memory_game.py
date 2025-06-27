d478bbea04f91c3df3e32f9746e3fca90dde7c93
eliadnomt <eliadnomt@gmail.com>
2025-06-27 13:02:07 +0200

    Refactored to use class structure in memory_game

--- memory_game.py
+++ memory_game.py
@@ -6,98 +6,171 @@ import math
 import copy
 from display_utils import card_displayer
 
+# memory_game.py
+"""
+Memory game functionality
+"""
+from random import randrange
+import math
+import copy
+from display_utils import card_displayer
+
+
+class MemoryGame:
+	"""Class to handle memory game functionality"""
+
+	def __init__(self, deck_dict):
+		"""
+		Initialize the memory game with a deck of flashcards
+
+		:param deck_dict: a dictionary containing pairs of flashcards
+		"""
+		self.deck_dict = deck_dict
+		self.initial_pairs = [[key, value] for key, value in deck_dict.items()]
+		self.f_b_pairs = []
+		self.grid = []
+		self.rows_count = 0
+		self.columns_count = 0
+		self.game_round = 0
+
+	def choose_game_length(self):
+		"""Allow user to choose how many cards to include in the game"""
+		max_cards = len(self.initial_pairs)
+		length_chosen = False
+
+		while not length_chosen:
+			game_length = input(
+				f"Please choose how many cards you'd like to include in the game, between 2 and {min(24, max_cards)}: ")
+			if game_length.isdigit():
+				game_length_int = int(game_length)
+				if 2 <= game_length_int <= min(24, max_cards):
+					length_chosen = True
+					return game_length_int
+				else:
+					print(f"Please pick a number between 2 and {min(24, max_cards)}")
+			else:
+				print("Please enter a valid number")
+
+	def select_cards(self, game_length):
+		"""Select random cards from the deck for the game"""
+		self.f_b_pairs = []
+		available_pairs = copy.deepcopy(self.initial_pairs)
+
+		while len(self.f_b_pairs) < game_length:
+			quest_to_add = available_pairs.pop(randrange(len(available_pairs)))
+			self.f_b_pairs.append(quest_to_add)
+
+	def calculate_board_size(self, game_length):
+		"""Calculate optimal board dimensions"""
+		total_cards = game_length * 2
+		self.columns_count = math.floor(math.sqrt(total_cards))
+		self.rows_count = math.ceil(total_cards / self.columns_count)
+
+	def create_grid(self):
+		"""Create and populate the game grid"""
+		self.grid = [["0" for _ in range(self.columns_count)] for _ in range(self.rows_count)]
+
+		# Create a list of all cards (each pair appears twice)
+		all_cards = []
+		for pair in self.f_b_pairs:
+			all_cards.extend(pair)
+
+		# Fill grid randomly
+		for card in all_cards:
+			placed = False
+			while not placed:
+				target_row = randrange(self.rows_count)
+				target_col = randrange(self.columns_count)
+				if self.grid[target_row][target_col] == "0":
+					self.grid[target_row][target_col] = card
+					placed = True
+
+	def get_card_choice(self, prompt, excluded_card=None):
+		"""Get a valid card choice from the user"""
+		valid_choice = False
+		while not valid_choice:
+			choice = input(prompt)
+			if len(choice) == 2 and choice[0].isdigit() and choice[1].isalpha():
+				row = int(choice[0]) - 1
+				col = ord(choice[1]) - 97
+
+				if 0 <= row < self.rows_count and 0 <= col < self.columns_count:
+					chosen_card = self.grid[row][col]
+					if chosen_card != "0" and chosen_card != excluded_card:
+						return choice, chosen_card
+
+			print("Invalid choice. Please try again.")
+
+	def check_match(self, card1, card2):
+		"""Check if two cards form a matching pair"""
+		for pair in self.f_b_pairs:
+			if card1 in pair and card2 in pair:
+				return True
+		return False
+
+	def remove_matched_cards(self, choice1, choice2):
+		"""Remove matched cards from the grid"""
+		row1, col1 = int(choice1[0]) - 1, ord(choice1[1]) - 97
+		row2, col2 = int(choice2[0]) - 1, ord(choice2[1]) - 97
+		self.grid[row1][col1] = "0"
+		self.grid[row2][col2] = "0"
+
+	def is_game_complete(self):
+		"""Check if all cards have been matched"""
+		for row in self.grid:
+			for card in row:
+				if card != "0":
+					return False
+		return True
+
+	def play(self):
+		"""Main game loop"""
+		if len(self.initial_pairs) < 2:
+			print("Not enough cards in deck to play memory game. Need at least 2 pairs.")
+			return
+
+		# Setup game
+		game_length = self.choose_game_length()
+		self.select_cards(game_length)
+		self.calculate_board_size(game_length)
+		self.create_grid()
+
+		print("Pick two cards. Try to find the pairs!\nWhen guessing, input row # and column letter")
+		print("eg. Row 3, Column b would be '3b'")
+
+		self.game_round = 0
+
+		while not self.is_game_complete():
+			self.game_round += 1
+			print_grid(self.grid, self.columns_count)
+
+			# Get first card choice
+			choice1, card1 = self.get_card_choice("Please pick a card to turn over: ")
+			print(card_displayer(card1))
+
+			# Get second card choice
+			choice2, card2 = self.get_card_choice("Try to find the match!: ", excluded_card=card1)
+			print(card_displayer(card2))
+
+			# Check for match
+			if self.check_match(card1, card2):
+				print("Congratulations! You found a match!")
+				self.remove_matched_cards(choice1, choice2)
+			else:
+				print("Sorry, please try again.")
+
+		print(f"Congratulations, you won! It took you {self.game_round} rounds.")
+
 
 def memory_game(deck_dict):
-    """
-    Plays a game of 'memory'
+	"""
+	Plays a game of 'memory' using the MemoryGame class
 
-    :param deck_dict: a dictionary containing pairs of flashcards.
-    """
-    # Choose cards from deck
-    # Converts dict into list of key/value tuples
-    initial_pairs0 = [(i for i in pair) for pair in deck_dict.items()]
-    initial_pairs = [[i for i in pair] for pair in initial_pairs0]
-    # Make user choose quiz length
-    length_chosen = False
-    while not length_chosen:
-        game_length = input("Please choose how many cards you'd like to include in the game, between 5 and 24")
-        if game_length.isdigit():
-            if 24 >= int(game_length) >= 5 and int(game_length) < len(initial_pairs):
-                length_chosen = True
-        else:
-            print("Please pick again")
-    # Add as many questions to the quiz as the user had specified
-    f_b_pairs = []
-    while len(f_b_pairs) < int(game_length):
-        quest_to_add = initial_pairs[randrange(len(initial_pairs))]
-        if quest_to_add not in f_b_pairs:
-            f_b_pairs.append(quest_to_add)
-    # Calculate the board size
-    # Columns_count will be width, rows_count height
-    columns_count = math.floor(math.sqrt(int(game_length) * 2))
-    rows_count = math.ceil((int(game_length) * 2) / columns_count)
-    # Because we pop out elements of the list when filling the grid, we need to copy for later reference.
-    f_b_pairs1 = copy.deepcopy(f_b_pairs)
+	:param deck_dict: a dictionary containing pairs of flashcards
+	"""
+	game = MemoryGame(deck_dict)
+	game.play()
 
-    grid = grid_maker(rows_count, columns_count)
-    # Fill grid, make sure the target location is empty ("0"), pop the itm and fill till each item in the list is empty
-    for itm4 in f_b_pairs:
-        while len(itm4) > 0:
-            target_row = randrange(rows_count)
-            target_col = randrange(columns_count)
-            target = grid[target_row][target_col]
-            if target == "0":
-                grid[target_row][target_col] = itm4.pop()
-
-    # Start the game here
-    print("Pick two cards. Try to find the pairs!\nWhen guessing, input row # and column letter"
-          "\neg. Row 3, Column b would be '3b' ")
-    game_round = 0
-    game_over = False
-    while not game_over:
-        game_round += 1
-        print_grid(grid, columns_count)
-        # Pick first card
-        valid_choice = False
-        while not valid_choice:
-            choice1 = input("Please pick a card to turn over:   ")
-            if choice1[0].isdigit() and choice1[1].isalpha():
-                if len(choice1) == 2 and int(choice1[0]) <= rows_count and ord(choice1[1]) < columns_count + 97:
-                    choice1_picked = grid[int(choice1[0]) - 1][ord(choice1[1]) - 97]
-                    if choice1_picked != "0":
-                        valid_choice = True
-
-        print(card_displayer(choice1_picked))
-        # Pick second card
-        valid_choice2 = False
-        while not valid_choice2:
-            choice2 = input("Try to find the match!:   ")
-            if choice2[0].isdigit() and choice2[1].isalpha():
-                if len(choice2) == 2 and int(choice2[0]) <= rows_count and ord(choice2[1]) < columns_count + 97:
-                    choice2_picked = grid[int(choice2[0]) - 1][ord(choice2[1]) - 97]
-                    if choice2_picked != "0" and choice2_picked != choice1_picked:
-                        valid_choice2 = True
-        print(card_displayer(choice2_picked))
-        is_match = False
-        for itm5 in f_b_pairs1:
-            if choice1_picked in itm5 and choice2_picked in itm5:
-                is_match = True
-
-        if is_match:
-            print("Congratulations! You found a match!")
-            grid[int(choice1[0]) - 1][ord(choice1[1]) - 97] = "0"
-            grid[int(choice2[0]) - 1][ord(choice2[1]) - 97] = "0"
-        else:
-            print("Sorry, please try again.")
-
-        for row in grid:
-            for itm6 in row:
-                if itm6 == "0":
-                    game_over = True
-                else:
-                    game_over = False
-        if game_over:
-            print(f"Congratulations, you won! It took you {str(game_round)} rounds.")
 
 def grid_maker(h, w):
 	grid1 = [["0" for i in range(w)] for i in range(h)]
